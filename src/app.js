702b74cfe6f3fab8a92207d87c3a9527ab53a5a6
abhisheksonkar20 <sonkar.abhishek45@gmail.com>
2020-12-08 21:10:49 +0530

    updated code

--- src/app.js
+++ src/app.js
@@ -1,12 +1,38 @@
 
 const express = require('express');
-const app = express()
-
+const app = express();
+const SubscriberModel = require('./models/subscribers');
 
 // Your code goes here
+app.get('/subscribers', async(req, res) => {
+    res.send(await SubscriberModel.find());
+});
 
+app.get('/subscribers/names', async(req, res) => {
+    const allData = await SubscriberModel.find();
+    const requiredData = allData.map(data => {
+        return({
+            name: data.name,
+            subscribedChannel: data.subscribedChannel
+        });
+    });
+    res.send(requiredData);
+});
 
-
+app.get('/subscribers/:id', async(req, res) => {
+    const searchId = req.params.id;
+    try {
+        const reqData = await SubscriberModel.findOne( { _id: searchId });
+        if(reqData == null) {
+            res.sendStatus(400).send({ message: "Not found" });
+        } else {
+            res.send(reqData);
+        }
+    }
+    catch(err) {
+        res.sendStatus(400).send({ message: err.message });
+    }
+});
 
 
 
